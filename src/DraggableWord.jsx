b60dbf69372a73490d7e016bf955fa42886e11c0
Lucia .C <70353637+lucia20200818@users.noreply.github.com>
2024-06-20 20:10:12 +0100

    Update DraggableWord.jsx

--- src/DraggableWord.jsx
+++ src/DraggableWord.jsx
@@ -3,15 +3,14 @@ import React from 'react';
 import Draggable from 'react-draggable';
 import './DraggableWord.scss';
 
-const DraggableWord = ({ word }) => {
-  const handleClick = (e) => {
-   
-  };
-
+const DraggableWord = ({ word, onDelete }) => {
   return (
     <Draggable>
-      <div className="draggable-word" onClick={handleClick}>
+      <div className="draggable-word">
         {word}
+        <button className="delete-word-button" onClick={onDelete}>
+          ✕
+        </button>
       </div>
     </Draggable>
   );
